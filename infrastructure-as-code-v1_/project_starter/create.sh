b68964b450c77b7dc4d136474dbde19494dfb1ad
Iyusuf40 <punkgts40@gmail.com>
2022-12-11 13:38:54 +0000

    test

--- infrastructure-as-code-v1_/project_starter/create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-aws cloudformation create-stack --stack-name cloudformation-project --template-body \
-	file://final-project-starter.yml \
-	--parameters file://server-parameters.json \
-        --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
--- infrastructure-as-code-v1_/project_starter/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-aws cloudformation update-stack --stack-name cloudformation-project \
-       	--template-body file://final-project-starter.yml \
-	--parameters file://server-parameters.json \
-        --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
